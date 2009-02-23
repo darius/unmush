17a0b63a311421ec9f32411a3de9206cf85232ba
darius <darius@66-214-133-108.dhcp.gldl.ca.charter.com>
2009-02-23 14:17:32 -0800

    tweaks to viterbi algorithm and unknown-word model

--- viterbi.py
+++ viterbi.py
@@ -9,22 +9,22 @@ def try_to_split(wordsruntogether):
 def viterbi_segment(text):
     "Return the lowest-cost segmentation of the text, and its cost."
     # Adapted from Russell & Norvig, AIMA.
-    # The best segmentation of text[:i] will have cost cost[i] and
-    # last word text[word[i]:i]. Fill them in by dynamic programming:
-    cost, word = [0.0], [0]
+    # The best segmentation of text[:i] will have cost costs[i] and
+    # last word text[lasts[i]:i]. Fill them in by dynamic programming:
+    costs, lasts = [0.0], [0]
     for i in range(1, len(text) + 1):
-        cost_k, k = min((cost[j] + word_cost(text[j:i]), j)
+        cost_k, k = min((costs[j] + word_cost(text[j:i]), j)
                         for j in range(max(0, i - max_word_length), i))
-        cost.append(cost_k)
-        word.append(k)
+        costs.append(cost_k)
+        lasts.append(k)
     # Trace back the words of the best segmentation:
     words = []
     i = len(text)
     while 0 < i:
-        words.append(text[word[i]:i])
-        i = word[i]
+        words.append(text[lasts[i]:i])
+        i = lasts[i]
     words.reverse()
-    return words, cost[-1]
+    return words, costs[-1]
 
 
 # The unigram cost model.
@@ -32,16 +32,24 @@ def viterbi_segment(text):
 # For others, we try to stay roughly consistent.
 
 def word_cost(word):
-    try:
+    if word in dictionary:
         return dictionary[word]
-    except KeyError:
-        if word.isdigit():
-            return 10
-        elif any(letter.isdigit() for letter in word):
-            return 1e6
-        else:
-            # TODO: use n-gram frequencies
-            return 10 + 2.4 * len(word)
+    if word.isalpha() or all(letter.isalpha() or 128 <= ord(letter)
+                             for letter in word):
+        # TODO: use n-gram frequencies
+#       return 10 + letter_cost * len(word)
+        # XXX these numbers are still kind of fucked:
+        #  e.g. mm f -> mmf, jan -> j an
+        #       heb -> he b
+        #       sonyh1 -> so nyh 1
+        return per_word_cost + letter_cost * len(word)
+    if any(letter.isalpha() for letter in word):
+        return 1e6
+    if word.isdigit():
+        return 10
+    if any(letter.isdigit() for letter in word):
+        return 1e6
+    return 10
 
 
 ## The dictionary
@@ -62,3 +70,13 @@ def normalize(word):
 
 dictionary = read_dictionary(open('freqlist.txt'))
 max_word_length = max(map(len, dictionary))
+
+
+# A letter-cost just a bit under the cost of the most common 1-letter
+# word, to keep word-jumbles from getting split wherever they happen
+# to have an 'a', etc.
+#letter_cost = dictionary['a'] - 0.06093
+letter_cost = dictionary['a'] - 0.01
+
+per_word_cost = max(dictionary.values())
+#print per_word_cost, letter_cost
